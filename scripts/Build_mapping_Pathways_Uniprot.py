f7eb393a7ef988c646661b195b64331dfcc7fcc6
George_Weingart <George_Weingart@localhost>
2014-10-17 15:06:09 -0400

    humann2_test:  Modified the program that generates the mapping Pathway--> UniprotId by adding documentation and making sure the pathway does not contains tab delimiter so it is the first field in the extract

--- scripts/Build_mapping_Pathways_Uniprot.py
+++ scripts/Build_mapping_Pathways_Uniprot.py
@@ -10,7 +10,29 @@ import math
 import pdb
 import tempfile 
 import argparse
-from subprocess import call
+
+
+
+#********************************************************************************************
+#    Map Pathways to Uniprot IDs                                                            *
+#                                                                                           *
+#    The objective of this program is to map  Swissprot Pathways to Uniprot ACs             *
+#                                                                                           *
+ 
+#  -----------------------------------------------------------------------------------------*
+#  Invoking the program:                                                                    *
+#  ---------------------                                                                    *
+#  python Build_mapping_Pathways_Uniprot.py --i /n/huttenhower_lab_nobackup/downloads/uniprot_pathways/2014_10/pathway.txt --o  unipathway_uniprots.uniref 
+#                                                                                           *
+#   Where:                                                                                  *
+#    --i_reactions, is the pathways  file, which is currently located at                    *
+#    /n/huttenhower_lab_nobackup/downloads/uniprot_pathways/2014_10/pathway.txt             *
+#  and it was downloaded from  the site:  http://www.uniprot.org/help/pathway               *
+#                                                                                           *
+#   Written by George Weingart  Oct. 17, 2014   george.weingart@gmail.com                   *
+#********************************************************************************************
+
+
 
 
 #*************************************************************************************
@@ -19,29 +41,12 @@ from subprocess import call
 def read_params(x):
 	CommonArea = dict()	
 	parser = argparse.ArgumentParser(description='Build mapping Pathways to Uniprot Ids')
-	parser.add_argument('--o', action="store", dest='o',nargs='?',  default='summary_analysis')
+	parser.add_argument('--o', action="store", dest='o',nargs='?')
 	parser.add_argument('--i', action="store", dest='i',nargs='?')
 	CommonArea['parser'] = parser
 	return  CommonArea
  
  
-#*************************************************************************************
-#* Filter the records                                                                *
-#*************************************************************************************
-def FilterLine(iLine):
-		if len(iLine) == 0\
-		or iLine.startswith("----")\
-		or iLine.find("UniProt - Swiss-Prot Protein Knowledgebase") > -1 \
-		or iLine.find("SIB Swiss Institute of Bioinformatics; Geneva, Switzerland") > -1\
-		or  iLine.find("European Bioinformatics Institute (EBI); Hinxton, United Kingdom") > -1\
-		or iLine.find("Protein Information Resource (PIR); Washington DC, USA") > -1\
-		or iLine.find("Description: PATHWAY comments: index") > -1\
-		or iLine.find("Name:        pathway.txt") > -1\
-		or  iLine.find("Release:") > -1\
-		or iLine.find("Copyrighted by the UniProt Consortium") > -1\
-		or iLine.find("Distributed under the Creative Commons Attribution-NoDerivs License") > -1:
-			return -1
-		return 0
  
 
 #*************************************************************************************
@@ -62,6 +67,7 @@ def Map_Pathways_toUniprotIDs(CommonArea):
 			if len(lOutputLine) > 0:
 				strBuiltRecord = "\t".join(lOutputLine) + 	"\n"
 				OutputFile.write(strBuiltRecord )
+				iLine = iLine.replace("\t"," ",3)
 			lOutputLine = [iLine]
 		else:
 			lEntries = iLine.split()
@@ -82,6 +88,23 @@ def Map_Pathways_toUniprotIDs(CommonArea):
 
 
 
+#*************************************************************************************
+#* Filter the records                                                                *
+#*************************************************************************************
+def FilterLine(iLine):
+		if len(iLine) == 0\
+		or iLine.startswith("----")\
+		or iLine.find("UniProt - Swiss-Prot Protein Knowledgebase") > -1 \
+		or iLine.find("SIB Swiss Institute of Bioinformatics; Geneva, Switzerland") > -1\
+		or  iLine.find("European Bioinformatics Institute (EBI); Hinxton, United Kingdom") > -1\
+		or iLine.find("Protein Information Resource (PIR); Washington DC, USA") > -1\
+		or iLine.find("Description: PATHWAY comments: index") > -1\
+		or iLine.find("Name:        pathway.txt") > -1\
+		or  iLine.find("Release:") > -1\
+		or iLine.find("Copyrighted by the UniProt Consortium") > -1\
+		or iLine.find("Distributed under the Creative Commons Attribution-NoDerivs License") > -1:
+			return -1
+		return 0
 
 
 
