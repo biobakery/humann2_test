b2d983c63237d4a707ca9b3871fc7da9e598bef5
Lauren McIver <Lauren McIver@localhost>
2014-11-21 16:45:00 -0700

    Add tests for gzip input files and the gunzip function

--- src/basic_tests_utilities.py
+++ src/basic_tests_utilities.py
@@ -4,6 +4,9 @@ import os
 import filecmp
 import sys
 import logging
+import gzip
+import tempfile
+import shutil
 
 import cfg
 import utils
@@ -218,7 +221,92 @@ class TestHumann2UtilitiesFunctions(unittest.TestCase):
         
         self.assertEqual(format,"blastm8")
         
+    def test_determine_file_format_fasta_gzip(self):
+        """
+        Test the determine_file_format function with a fasta file that is gzipped
+        """
         
-            
+        # create a small gzipped fasta file
+        # read in the small fasta file
+        file_handle=open(cfg.small_fasta_file,"r")
+        
+        # create a temp file
+        file_out, gzip_fasta_file=tempfile.mkstemp(suffix=".gz")
+        os.close(file_out)
+        
+        # write the gzipped file
+        file_handle_gzip=gzip.open(gzip_fasta_file,"w")
+        shutil.copyfileobj(file_handle, file_handle_gzip)
+        file_handle.close()
+        file_handle_gzip.close()
+           
+        format=utilities.determine_file_format(gzip_fasta_file)
+        
+        # remove the temp gzipped file
+        utils.remove_temp_file(gzip_fasta_file)
+        
+        self.assertEqual(format,"fasta.gz")
+
+    def test_determine_file_format_fastq_gzip(self):
+        """
+        Test the determine_file_format function with a fastq file that is gzipped
+        """
+        
+        # create a small gzipped fastq file
+        # read in the small fastq file
+        file_handle=open(cfg.small_fastq_file,"r")
+        
+        # create a temp file
+        file_out, gzip_fastq_file=tempfile.mkstemp(suffix=".gz")
+        os.close(file_out)
+        
+        # write the gzipped file
+        file_handle_gzip=gzip.open(gzip_fastq_file,"w")
+        shutil.copyfileobj(file_handle, file_handle_gzip)
+        file_handle.close()
+        file_handle_gzip.close()
+           
+        format=utilities.determine_file_format(gzip_fastq_file)
+        
+        # remove the temp gzipped file
+        utils.remove_temp_file(gzip_fastq_file)
+        
+        self.assertEqual(format,"fastq.gz")
+        
+    def test_gunzip_file(self):
+        """
+        Test the gunzip_file function
+        """
+        
+        # create a small gzipped fastq file
+        # read in the small fastq file
+        file_handle=open(cfg.small_fastq_file,"r")
+        
+        # create a temp file
+        file_out, gzip_fastq_file=tempfile.mkstemp(suffix=".gz")
+        os.close(file_out)
+        
+        # write the gzipped file
+        file_handle_gzip=gzip.open(gzip_fastq_file,"w")
+        shutil.copyfileobj(file_handle, file_handle_gzip)
+        file_handle.close()
+        file_handle_gzip.close()
+           
+        # create the ungzipped file
+        new_file=utilities.gunzip_file(gzip_fastq_file)
+        
+        # remove the temp gzipped file
+        utils.remove_temp_file(gzip_fastq_file)
+    
+        # check the files are the same
+        self.assertTrue(filecmp.cmp(new_file,
+            cfg.small_fastq_file, shallow=False))
+        
+        # remove the temp gunzipped file
+        utils.remove_temp_file(new_file)
+        
+        
+
+         
     
         
